10bd2527eb4099a92a4b6ec6c5caf88d6540872b
martinschobben <m.a.n.schobben@uu.nl>
2021-01-15 16:41:18 +0100

    overhaul of structure; pulled apart functions with one wrapper main function

--- R/Cameca.R
+++ R/Cameca.R
@@ -8,13 +8,13 @@
 #' \code{Diag_R} is more convenient as it defines all the ion- and isotope-wise
 #' statistics required for the diagnostics.
 #'
-#' @param df A tibble with ion count data and statistics for ion- and
+#' @param .df A tibble with ion count data and statistics for ion- and
 #' isotope-wise statistics.
-#' @param args A list of quosures pertaining to the variables required for a
+#' @param .args A list of quosures pertaining to the variables required for a
 #' call to stat_R. The function expr_R can be used to simplify setting this
 #' argument.
 #' @param ... Variables for grouping.
-#' @param output Character string determining whether the returned values in a
+#' @param .output Character string determining whether the returned values in a
 #' minimal version `"flag"` (original dataset + diagnostics) or an extended
 #' version with all the intermediate steps of ion- and isotope-wise summary
 #' statistics `"complete"`.
@@ -28,69 +28,50 @@
 #' # Use point_example() to access the examples bundled with this package
 #'
 #' # raw data containing 13C and 12C counts on carbonate
-#' tb.rw <- read_IC(point_example("2018-01-19-GLENDON"))
+#' tb_rw <- read_IC(point_example("2018-01-19-GLENDON"))
 #'
 #' # Processing raw ion count data
-#' tb.pr <- cor_IC(tb.rw, N.rw, t.rw, det_type.mt)
+#' tb_pr <- cor_IC(tb_rw)
 #'
-#' # Descriptive an predictive statistics for 13C/12C ratios (note output
-#' # argument)
-#' tb.R <- stat_R(tb.pr, Xt.pr, N.pr, species.nm, ion1 = "13C",
-#'                ion2 = "12C", file.nm, bl.mt, output = "complete")
+#' # Descriptive an predictive statistics for 13C/12C ratios (note .output
+#' # argument and remove zero count analysis)
+#' tb_R <- stat_R(tb_pr, "13C", "12C", file.nm, sample.nm, .output = "complete", .zero = TRUE)
 #'
-#' # CAMECA style augmentatio of ion count data for isotope ratios
-#' tb.aug <- Cameca(tb.R,
-#'                  args = expr_R(Xt = "Xt.pr",
-#'                                N = "N.pr",
-#'                                species = "species.nm",
-#'                                ion1 = "13C",
-#'                                ion2 = "12C"
-#'                                ),
-#'                   file.nm,
-#'                   bl.mt,
-#'                   output = "flag"
-#'                   )
-Cameca <- function(df, args = expr_R(NULL), vars, ..., output){
+#' # Mimic stat_R call
+#' args <-  expr_R("Xt.pr", "N.pr", "species.nm", "13C", "12C")
+#'
+#' # CAMECA style augmentation of ion count data for isotope ratios
+#' tb_dia <- Cameca(tb_R, file.nm, sample.nm, .args = args, .output = "flag")
+Cameca <- function(.df, ..., .args = expr_R(NULL), .output){
 
+  # Grouping
   gr_by <- enquos(...)
 
-  # Switch output complete dataset, stats or summary stats
-  # mod_out <- function(output) {
-  #   switch(output,
-  #          flag = call2( "select", expr(.),
-  #                        expr(.data$ID),
-  #                        expr(.data$lower),
-  #                        expr(.data$upper),
-  #                        expr(.data$flag)
-  #          ),
-  #          complete = call2( "invisible", expr(.)))
-  # }
+  # New quosures
+  # Single ion (light)
+  Xt2 <- quo_updt(.args[["Xt"]], post = as_name(.args[["ion2"]]))
 
-  df_dia <- df %>%
-    group_by(!!! gr_by) %>%
-    mutate(lower = !! quo_updt(my_q = args[["Xt"]],
-                               x = "M_R") - 2 *
-             !! quo_updt(my_q = args[["Xt"]],
-                         x = "S_R"),
-           upper = !! quo_updt(my_q = args[["Xt"]],
-                               x = "M_R") + 2 *
-             !! quo_updt(my_q = args[["Xt"]],
-                         x = "S_R"),
-           hat_Y =  !! quo_updt(my_q = args[["Xt"]],
-                                x = "M_R") *
-             !! quo_updt(my_q = args[["Xt"]],
-                         txt = as_name(args[["ion2"]])
-                         ),
-           flag = if_else(
-             between(!! quo_updt(my_q = args[["Xt"]], x = "R"),
-                     unique(.data$lower),  # mean - 2SD
-                     unique(.data$upper)), # mean + 2SD
-             "good",
-             "bad"
-           ),
-           flag = as.factor(flag)
-    ) %>%
+  # R
+  R <- quo_updt(.args[["Xt"]], pre = "R")
+  M_R <- quo_updt(.args[["Xt"]], pre = "M_R")
+  S_R <- quo_updt(.args[["Xt"]], pre = "S_R")
+
+  group_by(.df, !!! gr_by) %>%
+    mutate(
+      lower = !! M_R - 2 * !! S_R ,
+      upper = !! M_R + 2 * !! S_R,
+      hat_Y = !! M_R * !! Xt2,
+      flag = if_else(
+        between(!! R,
+        unique(.data$lower),  # mean - 2SD
+        unique(.data$upper)), # mean + 2SD
+        "good",
+        "bad"
+        ),
+      flag = as.factor(flag)
+      ) %>%
     ungroup()
 
-  mod_out(df, df_dia, gr_by, args, output, vars)
+
+
 }
--- R/CooksD.R
+++ R/CooksD.R
@@ -1,202 +1,85 @@
-#' Ideal linear R model
+#' @rdname Cameca
 #'
 #' @export
-Rm <- function(df, args = expr_R(NULL), vars, ..., output){
+Rm <- norm_E <- CooksD <- QQ <- function(.df, ..., .args = expr_R(NULL), .output = "flag", .hyp = "none"){
 
-  # multicore
-  # cluster <- multidplyr::new_cluster(4)
-
+  # Grouping
   gr_by <- enquos(...)
 
-# heavy isotope
-  Xt1 <- quo_updt(args[["Xt"]], as_name(args[["ion1"]]))
-# light isotope
-  Xt2 <- quo_updt(args[["Xt"]], as_name(args[["ion2"]]))
+  # function name
+  fun_nm <- as_name(match.call()[[1]])
 
-# standard deviation of R
-  S_R <- quo_updt(my_q = args[["Xt"]], x = "S_R")
-
-  df_dia <- df %>%
-    group_by(!!! gr_by) %>%
-    tidyr::nest() %>% #multidplyr::partition(cluster)
-    mutate(R_lm = purrr::map(data, ~lm_form(.x, Xt1, Xt2, type = "Rm")),
-           model = purrr::map(R_lm, ~{broom::augment(.x) %>%
-               select(-c(!!Xt1, X =!!Xt2),
-                      )
-           }
-           )
-           ) %>%
-    select(-R_lm) %>%
-    tidyr::unnest(cols = c(data, model)) %>%
-    mutate(
-# Modelled Y values
-            hat_Y = .data$.fitted,
-            hat_min = hat_Y - 2 * sigma_calc(.data$.resid),
-            hat_max = hat_Y + 2 * sigma_calc(.data$.resid),
-            flag = if_else(!! Xt1 > hat_min &
-                           !! Xt1 < hat_max,
-                           "good",
-                           "bad"
-                           ),
-            flag = as.factor(flag)
-            ) %>%
-    # dplyr::collect()
-    ungroup()
-
-  mod_out(df, df_dia, gr_by, args, output, vars)
-
-}
-
-#' Normalization of residuals
-#'
-#' @export
-norm_E <- function(df, args = expr_R(NULL), vars, ..., output){
-
-  gr_by <- enquos(...)
-
-  df_dia<- Rm(df, args = args, vars, !!! gr_by, output = "augment") %>%
-    left_join(df, ., by = "ID") %>%
-    group_by(!!! gr_by) %>%
-    mutate(
-      studE = .data$.std.resid,
-      hat_Xi = .data$.hat,
-      CooksD = .data$.cooksd,
-      CooksD_cf = purrr::map_dbl(
-        !! quo_updt(my_q = args[["Xt"]], x = "n_R"),
-        cooksD_cut_calc
-       ),
-      flag = if_else(CooksD > CooksD_cf,
-                     "bad",
-                     "good"
-                     ),
-      flag = as.factor(flag)
-      ) %>%
-    ungroup()
-
-  mod_out(df, df_dia, gr_by, args, output, vars)
+  # check if name has hypothesis test
+  if(fun_nm != "QQ" & .hyp != "none"){
+    .hyp <- "none"
+    warning("No hypothesis test avalaible for this method")
   }
 
+  # Heavy isotope
+  Xt1 <- quo_updt(.args[["Xt"]], post = as_name(.args[["ion1"]]))
+  # Light isotope
+  Xt2 <- quo_updt(.args[["Xt"]], post = as_name(.args[["ion2"]]))
 
-#' @rdname Cameca_R
-#'
-#' @export
-CooksD <- function(df, args = expr_R(NULL), vars, ..., output){
+  # Execute
+  df <- nest_R_lm(.df, gr_by, Xt1, Xt2, method = fun_nm, .hyp = .hyp)
 
-  gr_by <- enquos(...)
+  # Output
+  if(.output == "flag") return(unnest(select(df, -c(data, aug)), cols = c(extr, flag)))
+  if(.output == "complete") return(unnest(select(df, -aug), cols = c(data, extr, flag)))
 
-  df_cd <- norm_E(df, args = args, vars, !!! gr_by, output = "flag") %>%
-    filter(!!args[["species"]] == !!args[["ion1"]]) %>%
-    select(-vars[["original"]])
-
-  df_rm <- Rm(df, args = args, vars, !!! gr_by, output = "complete") %>%
-    select(-flag)
-
-  df_dia <- left_join(df_rm, df_cd, by = c("ID", sapply(gr_by, as_name)))
-
-  mod_out(df, df_dia, gr_by, args, output, vars)
-}
-
-#' Normality test
-#'
-#' @export
-QQ <- function(df, args = expr_R(NULL), vars, ..., output){
-
-  gr_by <- enquos(...)
-
-  df_dia <- Rm(df, args = args, vars, !!! gr_by, output = "augment") %>%
-     left_join(df, ., by = "ID") %>%
-     group_by(!!! gr_by) %>%
-     mutate(prob_vc =  vector_probs(!! quo_updt(my_q = args[["Xt"]],
-                                               x = "n_R"
-                                               )
-                                  ),
-            RQ = unname(quantile(.data$.std.resid, probs = prob_vc)),
-# Calculate normal (Theoretical) quantiles using mean and standard deviation from
-            TQ = qnorm(prob_vc, mean(RQ), sd(RQ)),
-# The standard error is calculated with,
-            hat_Y = mean(RQ) + sd(RQ) * TQ,
-            hat_RQ_se = hat_QR_se(RQ,
-                                  TQ,
-                                  prob_vc,
-                                  !! quo_updt(my_q = args[["Xt"]],
-                                              txt = as_name(args[["ion2"]]),
-                                              x = "n"
-                                              )
-                                  ),
-            hat_min =  hat_Y - 2 * hat_RQ_se,
-            hat_max =  hat_Y + 2 * hat_RQ_se,
-            flag = if_else(RQ > hat_min & RQ < hat_max, "good", "bad"),
-            flag = as.factor(flag),
-            flag_QQ = if_else(
-              (nortest::ad.test(.data$.std.resid))$p.value < 0.05,
-              "Ha (non-normal)",
-              "H0 (normal)"
-              ),
-            flag_QQ = as.factor(flag_QQ),
-# t-test flag for mu0 (aka the conditional mean of epsilon) being zero
-            flag_CM = if_else(
-              (t.test(.data$.std.resid, mu = 0))$p.value  < 0.05,
-              "Ha (mu0 is not zero)",
-              "H0 (mu0 is zero)"
-              ),
-            flag_CM = as.factor(flag_CM)
-            ) %>%
-            ungroup()
-
-  mod_out(df, df_dia, gr_by, args, output, vars)
 }
 
 
-#' Constant variance test
+
+#' @rdname Cameca
 #'
 #' @export
-CV <- function(df, args = expr_R(NULL), vars, ..., output){
+CV <- function(.df, .args = expr_R(NULL), .vars, ..., .output){
 
   gr_by <- enquos(...)
 
-# heavy isotope
-  Xt2 <- quo_updt(args[["Xt"]], as_name(args[["ion2"]]))
+  # heavy isotope
+  Xt2 <- quo_updt(.args[["Xt"]], post = as_name(.args[["ion2"]]))
+  n_R <- quo_updt(.args[["Xt"]], pre = "n", post = as_name(.args[["ion2"]]))
 
-# variable names of original dataset
-  var_names <- c(colnames(df), aug_names)
+  # build unique id
+  df <- ID_builder(.df, !!!gr_by, .species = R.nm)
+
+  # variable names of original dataset
+  var_names <- colnames(df)
   var_names <- var_names[!var_names %in% c("ID", sapply(gr_by, as_name))]
 
-  df_dia <- Rm(df, args = args, vars, !!! gr_by, output = "augment") %>%
+  df_dia <- Rm(df, args = .args, .vars, !!! gr_by, output = "augment") %>%
     left_join(df, ., by = "ID") %>%
     group_by(!!! gr_by) %>%
-# Hetroscadasticity test (Breusch Pagan test)(level of confidence 95%;
-# cut-off 0.05 for H0 rejection),
+    # Hetroscadasticity test (Breusch Pagan test)(level of confidence 95%;
+    # cut-off 0.05 for H0 rejection)
     tidyr::nest() %>%
     mutate(
       res_lm = purrr::map(data, ~lm_form(.x, quo(.std.resid), Xt2)),
       R2 = purrr::map(res_lm, ~{broom::glance(.x)$r.squared}),
       SE_beta = purrr::map(res_lm, ~{pull(broom::tidy(.x), std.error)[2]}),
-          ) %>%
+      ) %>%
     tidyr::unnest(cols = c(data, R2, SE_beta)) %>%
-    mutate(Chi_R2 = R2 * !! quo_updt(my_q = args[["Xt"]],
-                                      txt = as_name(args[["ion2"]]),
-                                      x = "n"
-                                      ),
-           flag_CV = if_else(Chi_R2 >
-                        qchisq(.95, df = 1),
-                        "Ha (heteroskedasticity)",
-                        "H0 (homoskedasticity)"
-                            ),
-           hat_min = -3.5,
-           hat_max = 3.5,
-           fitted = .data$.fitted,
-           studE = .data$.std.resid,
-           hat_Y = 0,
-           flag = if_else(studE  > hat_min &
-                            studE < hat_max,
-                          "good",
-                          "bad"
-                          ),
-           flag = as.factor(flag)
-           ) %>%
+    mutate(
+      Chi_R2 = R2 * !!n_R,
+      flag_CV =
+        if_else(Chi_R2 > qchisq(.95, df = 1),
+                "Ha (heteroskedasticity)",
+                "H0 (homoskedasticity)"
+                ),
+      hat_min = -3.5,
+      hat_max = 3.5,
+      fitted = .data$.fitted,
+      studE = .data$.std.resid,
+      hat_Y = 0,
+      flag =
+        if_else(studE  > hat_min & studE < hat_max, "good", "bad"),
+      flag = as.factor(flag)
+      ) %>%
     ungroup()
 
-  mod_out(df, df_dia, gr_by, args, output, vars)
+  mod_out(df, df_dia, gr_by, .args, .output, .vars)
 
 }
 
@@ -204,14 +87,14 @@ CV <- function(df, args = expr_R(NULL), vars, ..., output){
 #' Independence of residuals
 #'
 #' @export
-IR <- function(df, args = expr_R(NULL), time_steps, ..., plot = FALSE){
+IR <- function(.df, args = expr_R(NULL), time_steps, ..., plot = FALSE){
 
   time_steps <- enquo(time_steps)
   time_steps <- quo_updt(time_steps, as_name(args[["ion1"]]))
   gr_by <- enquos(...)
 
-  df.acf <- Rm(df, args = args, !!! gr_by, output = "augment") %>%
-    left_join(df, ., by = "ID") %>%
+  df.acf <- Rm(.df, args = args, !!! gr_by, output = "augment") %>%
+    left_join(.df, ., by = "ID") %>%
     group_by(!!! gr_by) %>%
     arrange(!! time_steps) %>%
     select(!!! gr_by, .data$.std.resid) %>%
@@ -252,9 +135,7 @@ IR <- function(df, args = expr_R(NULL), time_steps, ..., plot = FALSE){
 }
 
 
-#' Standard error of the regression
-#' @export
-sigma_calc <- function(res) sqrt(sum((res ^ 2)) / (length(res) - 1))
+
 
 
 #-------------------------------------------------------------------------------
@@ -280,43 +161,29 @@ acf_calc <- function(data){
 }
 
 
-# Switch output to flag, augmentation and complete
-mod_out <- function(df_R, df_dia, grps, args, output, vars) {
-
-  if (output == "flag"){
-    # diagnostics variables
-    new.vars <- colnames(df_dia)[!colnames(df_dia) %in% colnames(df_R)]
-    new.vars <- new.vars[!str_detect(new.vars, "\\.")]
-    # select and bind
-    flag_df <- bind_rows(select(df_dia ,.data$ID, !!!grps, !!! vars[["ion1"]], !!! new.vars),
-                         select(df_dia, .data$ID, !!!grps, !!! vars[["ion2"]], !!! new.vars)
-                         )
-    return(flag_df)
-  }
-  if (output == "augment"){
-
-    return(select(df_dia, starts_with(c("ID", "."))))
-
-  }
-
-  if (output == "complete"){
-
-    return(select(df_dia, -starts_with(c("."))))
-    }
-}
-
-#   switch(output,
-#          flag = call2("select",
-#                       expr(.),
-#                       # expr(!contains("."))
-#                       !!! parse_exprs(paste0("-", vars))
-#                       ),
-#          complete = call2( "invisible", expr(.)),
-#          augment =  call2("select",
-#                           expr(.),
-#                           expr(starts_with(c("ID", ".")))
-#          )
-#   )
+# # Switch output to flag, augmentation and complete
+# mod_out <- function(df_R, df_dia, grps, args, output, vars) {
+#
+#   if (output == "flag"){
+#     # diagnostics variables
+#     new.vars <- colnames(df_dia)[!colnames(df_dia) %in% colnames(df_R)]
+#     new.vars <- new.vars[!str_detect(new.vars, "\\.")]
+#     # select and bind
+#     flag_df <- bind_rows(select(df_dia ,.data$ID, !!!grps, !!! vars[["ion1"]], !!! new.vars),
+#                          select(df_dia, .data$ID, !!!grps, !!! vars[["ion2"]], !!! new.vars)
+#                          )
+#     return(flag_df)
+#   }
+#   if (output == "augment"){
+#
+#     return(select(df_dia, starts_with(".") | "ID"))
+#
+#   }
+#
+#   if (output == "complete"){
+#
+#     return(select(df_dia, -starts_with(c("."))))
+#     }
 # }
 
 
@@ -335,6 +202,123 @@ hat_Y_se <- function(sigma, hat_Xi){
   sigma * sqrt(hat_Xi)
 }
 
-# Cook's D cutt-off value calculator
-cooksD_cut_calc  <- function(n) {4 / (n - 2)}
 
+# nest lm (args as quos)
+nest_R_lm <- function(df, gr_by, Xt1, Xt2, method, .hyp){
+
+
+  group_by(df, !!! gr_by) %>%
+    tidyr::nest() %>%
+    mutate(
+      R_lm =
+        purrr::map(data, ~lm_form(.x, Xt1, Xt2, type = "Rm")),
+      aug =
+        purrr::map(R_lm, broom::augment),
+      extr =
+        purrr::map(aug, ~transmute_reg(.x, Xt1, Xt2, method)),
+      extr =
+        purrr::map(extr, ~QQ_trans(.x, method, .hyp = .hyp)),
+      flag =
+        purrr::map2(extr, aug, ~flag_set(.x, .y, Xt1, Xt2, type = method))
+      ) %>%
+    select(-c(R_lm)) %>%
+    ungroup()
+
+
+}
+
+
+# augment function transform  and rename variables to standards of point
+transmute_reg <- function(df, Xt1, Xt2, type) {
+
+  # predicted variable and sigma level boundaries
+  prefix <- c("hat", "hat_sl", "hat_su")
+  hat_args <- purrr::map(prefix, ~quo_updt(Xt1, pre = .x))
+  hat_nm <- paste(prefix, as_name(Xt1), sep = "_")
+
+  # model args
+  args <- quos(
+    !!hat_args[[1]] := .data$.fitted,
+    !!hat_args[[2]] := .data$.fitted - 2 * sigma_calc(.data$.resid),
+    !!hat_args[[3]] := .data$.fitted + 2 * sigma_calc(.data$.resid) ,
+    studE = .data$.std.resid,
+    hat_Xi = .data$.hat,
+    CooksD = .data$.cooksd
+    )
+
+  if (type == "Rm") args <- args[names(args) %in% hat_nm]
+  if (type == "norm_E") args <- args[!names(args) %in% hat_nm]
+  if (type == "CooksD") args <- args[names(args) %in% c(hat_nm, "CooksD")]
+  if (type == "QQ") args <- args["studE"]
+
+  transmute(df, !!!args)
+
+}
+
+flag_set <- function(df1, df2, Xt1, Xt2, type){
+
+  # predicted variable and sigma level boundaries
+  prefix <- c("hat", "hat_sl", "hat_su")
+  hat_args_sigma <- purrr::map(prefix, ~quo_updt(Xt1, pre = .x))
+
+  # predicted variable and standard error and CI boundaries
+  hat_args_CI <- paste(c("hat", "hat_se"," hat_cl", "hat_cu"), "RQ", sep = "_") %>%
+    parse_exprs()
+
+  if (type == "Rm") {
+     df <- mutate(df1, !! Xt1 := pull(df2, !! Xt1)) %>%
+       transmute(flag = as.factor(if_else(between(!! Xt1, !! hat_args_sigma[[2]], !! hat_args_sigma[[3]]), "good", "bad")))
+    return(df)
+    }
+  if (type == "CooksD" | type == "norm_E") {
+    df <- transmute(df1, flag = as.factor(if_else(CooksD < {4 / (n() - 2)}, "good", "bad")))
+    return(df)
+    }
+  if (type == "QQ") {
+    df <- transmute(df1, flag = as.factor(if_else(!between(RQ, !!hat_args_CI[[3]], !!hat_args_CI[[4]]), "good", "bad")))
+    return(df)
+    }
+
+}
+
+
+QQ_trans <- function(df, type, .hyp) {
+
+  if (type!= "QQ") return(df)
+
+  # predicted variable and standard error and CI boundaries
+  hat_args <- paste(c("hat", "hat_se"," hat_cl", "hat_cu"), "RQ", sep = "_") %>%
+    parse_exprs()
+
+  # Normality hypothesis test
+  if (.hyp == "norm") {
+    hyp_result <- nortest::ad.test(df$studE)$p.value
+    Ha <- "Ha (non-normal)"
+    H0 <- "H0 (normal)"
+  }
+  # t-test flag for mu0 (aka the conditional mean of residual) being zero
+  if (.hyp == "ttest") {
+    hyp_result <- t.test(df$studE, mu = 0)$p.value
+    Ha <- "Ha (mu0 is not zero)"
+    H0 <- "H0 (mu0 is zero)"
+  }
+
+  if (.hyp == "none") hyp_result <- NULL; Ha = NULL; H0 = NULL
+
+  df <- transmute(
+    df,
+    RQ = unname(quantile(studE, probs = vector_probs(n()))),
+    # Calculate normal (Theoretical) quantiles using mean and standard deviation
+    TQ = qnorm(vector_probs(n()), mean(RQ), sd(RQ)),
+    # The standard error
+    !!hat_args[[1]] := mean(RQ) + sd(RQ) * TQ,
+    !!hat_args[[2]] := hat_QR_se(RQ, TQ, vector_probs(n()), n()),
+    !!hat_args[[3]] := !!hat_args[[1]] - 2 * !!hat_args[[2]],
+    !!hat_args[[4]] := !!hat_args[[1]] + 2 * !!hat_args[[2]],
+    hyp = if_else(hyp_result < 0.05, Ha, H0)
+    )
+
+}
+
+
+sigma_calc <- function(res) sqrt(sum((res ^ 2)) / (length(res) - 1))
