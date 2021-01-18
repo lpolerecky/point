1f7db82fa98dafa795763c0211a281eb56673d7b
martinschobben <m.a.n.schobben@uu.nl>
2021-01-18 20:09:09 +0100

    IR updated

--- R/CooksD.R
+++ R/CooksD.R
@@ -1,10 +1,9 @@
 #' @rdname Cameca
 #'
 #' @export
-CV <- Rm <- norm_E <- CooksD <- QQ <- function(
-  .df, .ion1, .ion2, ..., .Xt = Xt.pr, .t = t.nm, .output = "flag",
-  .hyp = "none"
-  ){
+CV <- Rm <- norm_E <- CooksD <- QQ <- IR <- function(
+  .df, .ion1, .ion2, ..., .Xt = Xt.pr, .t = t.nm,
+  .output = "complete", .hyp = "none"){
 
   # Grouping
   gr_by <- enquos(...)
@@ -37,128 +36,57 @@ CV <- Rm <- norm_E <- CooksD <- QQ <- function(
   df <- nest_R_lm(.df, gr_by, Xt1, Xt2, t, method = fun_nm, .hyp = .hyp)
 
   # Output
+  if (fun_nm == "IR") return(unnest(select(df, -c(t, data)), cols = c(extr, flag)))
   if (.output == "flag") return(unnest(select(df, -data), cols = c(t, extr, flag)))
   if (.output == "complete") return(unnest(select(df, -t), cols = c(data, extr, flag)))
-  # if (.output == "nest") return(df)
 }
-
-
-#' Independence of residuals
-#'
-#' @export
-IR <- function(.df, args = expr_R(NULL), time_steps, ..., plot = FALSE){
-
-  time_steps <- enquo(time_steps)
-  time_steps <- quo_updt(time_steps, as_name(args[["ion1"]]))
-  gr_by <- enquos(...)
-
-  df.acf <- Rm(.df, args = args, !!! gr_by, output = "augment") %>%
-    left_join(.df, ., by = "ID") %>%
-    group_by(!!! gr_by) %>%
-    arrange(!! time_steps) %>%
-    select(!!! gr_by, .data$.std.resid) %>%
-    mutate(LB_test = stats::Box.test(.data$.std.resid, type = "Ljung-Box")$p.value,
-           flag_IR = if_else(
-             LB_test < 0.05,
-             "Ha (dependence of residuals)",
-             "H0 (independence of residuals)"
-           ),
-           flag_IR = as.factor(flag_IR)
-           ) %>%
-    tidyr::nest(data = .data$.std.resid) %>%
-    mutate(ACF = purrr::map(data, acf_calc)) %>%
-    select(-data) %>%
-    ungroup() %>%
-    tidyr::unnest(cols = c(ACF))
-
-  if (plot) {
-
-  df.acf %T>%
-      { print(ggplot(., mapping = aes(x = lag, y = acf)) +
-                geom_hline(aes(yintercept = 0)) +
-                geom_segment(mapping = aes(xend = lag, yend = 0)) +
-                geom_hline(aes(yintercept = ci_upper), color = "darkblue") +
-                geom_hline(aes(yintercept = ci_lower), color = "darkblue") +
-                facet_wrap(vars(!!! gr_by), scales = "free") +
-                ggtitle("ACF plot") +
-                theme_classic()
-              )
-        }
-
-  } else {
-
-    return(df.acf)
-  }
-
-
-}
-
-
-
-
 
 #-------------------------------------------------------------------------------
 # Not exportet helper functions
 #-------------------------------------------------------------------------------
 
-# calculate autocorrelation
-acf_calc <- function(data){
-  acf  <- acf(data$.std.resid, plot = FALSE)
-  ci <- qnorm((1 - 0.95) / 2) / sqrt(length(data$.std.resid))
-  df.acf <- tibble(
-    lag = as.vector(acf$lag)[-1],
-    acf = as.vector(acf$acf)[-1],
-    ci_upper = ci,
-    ci_lower = -ci
-  )
-}
-
-
-
-
-
 # nest lm (args as quos)
 nest_R_lm <- function(df, gr_by, Xt1, Xt2, t, method, .hyp){
 
-
-  #group_by(df, ) %>%
   tidyr::nest(df, t = !!t, data = -c(!!! gr_by)) %>%
   mutate(
     R_lm =
       purrr::map(data, ~lm_form(.x, Xt1, Xt2, type = "Rm")),
     aug =
-        purrr::map(R_lm, broom::augment),
+      purrr::map(R_lm, broom::augment),
     extr =
       purrr::map(aug, ~transmute_reg(.x, Xt1, Xt2, method)),
     extr =
       purrr::map(extr, ~QQ_trans(.x, method, .hyp = .hyp)),
     extr =
+      purrr::map(extr, ~IR_trans(.x, method, .hyp = .hyp)),
+    extr =
       purrr::map2(aug, extr, ~bp_wrap(.x, .y, Xt2, method, .hyp = .hyp)),
     flag =
       purrr::map2(extr, aug, ~flag_set(.x, .y, Xt1, Xt2, type = method))
     ) %>%
-  select(-c(R_lm, aug)) #%>%
-    #ungroup()
-
+  select(-c(R_lm, aug))
 
 }
 
+# prefix for modelled values
+prefix <- c("hat", "hat_l", "hat_u")
 
 # augment function transform  and rename variables to standards of point
 transmute_reg <- function(df, Xt1, Xt2, type) {
 
   # predicted variable and sigma level boundaries
-  prefix <- c("hat", "hat_sl", "hat_su")
   hat_args <- purrr::map(prefix, ~quo_updt(Xt1, pre = .x))
   hat_nm <- paste(prefix, as_name(Xt1), sep = "_")
 
   # predicted sigma level boundaries studentized residuals
-  hat_args_studE_nm <- paste(c("hat", "hat_sl", "hat_su"), "studE", sep = "_")
+  hat_args_studE_nm <- paste(prefix, "studE", sep = "_")
   hat_args_studE <- parse_exprs(hat_args_studE_nm)
 
 
   # model args
   args <- quos(
+    original = !!Xt1,
     !!hat_args[[1]] := .data$.fitted,
     !!hat_args[[2]] := .data$.fitted - 2 * sigma_calc(.data$.resid),
     !!hat_args[[3]] := .data$.fitted + 2 * sigma_calc(.data$.resid) ,
@@ -170,14 +98,13 @@ transmute_reg <- function(df, Xt1, Xt2, type) {
     !!hat_args_studE[[3]] :=  3.5
     )
 
-  if (type == "Rm") args <- args[names(args) %in% hat_nm]
+  if (type == "Rm") args <- args[names(args) %in% c(hat_nm, "original")]
   if (type == "norm_E") args <- args[!names(args) %in% hat_nm]
   if (type == "CooksD") args <- args[names(args) %in% c(hat_nm, "CooksD")]
   if (type == "CV") args <- args[names(args) %in% c(hat_nm[1], "studE", hat_args_studE_nm)]
-  if (type == "QQ") args <- args["studE"]
+  if (type == "QQ"| type == "IR") args <- args["studE"]
 
   transmute(df, !!!args)
-
 
 }
 
@@ -185,33 +112,42 @@ transmute_reg <- function(df, Xt1, Xt2, type) {
 flag_set <- function(df1, df2, Xt1, Xt2, type){
 
   # predicted variable and sigma level boundaries
-  prefix <- c("hat", "hat_sl", "hat_su")
+  #prefix <- c("hat", "hat_sl", "hat_su")
   hat_args_sigma <- purrr::map(prefix, ~quo_updt(Xt1, pre = .x))
 
   # predicted variable and standard error and CI boundaries
-  hat_args_CI <- paste(c("hat", "hat_cl", "hat_cu"), "RQ", sep = "_") %>%
+  hat_args_CI <- paste(prefix, "RQ", sep = "_") %>%
     parse_exprs()
 
   # predicted sigma level boundaries studentized residuals
-  hat_args_studE <- paste(c("hat", "hat_sl", "hat_su"), "studE", sep = "_") %>%
+  hat_args_studE <- paste(prefix, "studE", sep = "_") %>%
     parse_exprs()
 
+  # predicted variable and standard error and CI boundaries
+  hat_args_acf <- paste(prefix, "acf", sep = "_") %>%
+    parse_exprs()
 
   if (type == "Rm") {
      df <- mutate(df1, !! Xt1 := pull(df2, !! Xt1)) %>%
-       transmute(flag = as.factor(if_else(between(!! Xt1, !! hat_args_sigma[[2]], !! hat_args_sigma[[3]]), "good", "bad")))
+       transmute(flag = purrr::pmap_chr(list(a = !!Xt1, b = !! hat_args_sigma[[2]],c = !! hat_args_sigma[[3]]), function(a,b,c)if_else(between(a,b,c), "confluent", "divergent")))
     return(df)
     }
   if (type == "CooksD" | type == "norm_E") {
-    df <- transmute(df1, flag = as.factor(if_else(CooksD < {4 / (n() - 2)}, "good", "bad")))
+    df <- transmute(df1, flag = as.factor(if_else(CooksD < {4 / (n() - 2)}, "confluent", "divergent")))
     return(df)
     }
   if (type == "QQ") {
-    df <- transmute(df1, flag = as.factor(if_else(!between(RQ, !!hat_args_CI[[2]], !!hat_args_CI[[3]]), "good", "bad")))
+    df <- transmute(rowwise(df1), flag = as.factor(if_else(between(RQ, !!hat_args_CI[[2]], !!hat_args_CI[[3]]), "confluent", "divergent")))
     return(df)
     }
   if (type == "CV") {
-    df <- transmute(df1, flag = as.factor(if_else(between(studE, !!hat_args_studE[[2]], !!hat_args_studE[[3]]), "good", "bad")))
+    df <- transmute(df1, flag = as.factor(if_else(between(studE, !!hat_args_studE[[2]], !!hat_args_studE[[3]]), "confluent", "divergent")))
+    return(df)
+    }
+  if (type == "IR") {
+    df <- transmute(
+      df1,
+      flag = factor(if_else(between(!! hat_args_acf[[1]], !!hat_args_acf[[2]], !!hat_args_acf[[3]]), "confluent", "divergent")))
     return(df)
     }
 
@@ -223,7 +159,7 @@ QQ_trans <- function(df, type, .hyp) {
   if (type!= "QQ") return(df)
 
   # predicted variable and standard error and CI boundaries
-  hat_args <- paste(c("hat", "hat_cl", "hat_cu"), "RQ", sep = "_") %>%
+  hat_args <- paste(prefix, "RQ", sep = "_") %>%
     parse_exprs()
 
   # Normality hypothesis test
@@ -239,7 +175,6 @@ QQ_trans <- function(df, type, .hyp) {
     H0 <- "H0 (mu0 is zero)"
   }
 
-  # if (.hyp == "none") hyp_result <- NULL; Ha = NULL; H0 = NULL
 
   df <- transmute(
     df,
@@ -259,6 +194,41 @@ QQ_trans <- function(df, type, .hyp) {
       }
 
 }
+
+# auto-correlation and hypothesis tests
+IR_trans <- function(df, type, .hyp) {
+
+  if (type!= "IR") return(df)
+
+  # independence test
+  if (.hyp == "ljung") {
+    hyp_result <- stats::Box.test(df$studE, type = "Ljung-Box")$p.value
+    Ha <- "Ha (dependence of residuals)"
+    H0 <- "H0 (independence of residuals)"
+  }
+
+  # predicted variable and standard error and CI boundaries
+  hat_args <- paste(prefix, "acf", sep = "_") %>%
+    parse_exprs()
+
+  acf <- acf(df$studE, plot = FALSE)
+  si <- qnorm((1 - 0.95) / 2) / sqrt(length(df$studE))
+
+  df <- tibble(
+    lag = as.vector(acf$lag)[-1],
+    !!hat_args[[1]] := as.vector(acf$acf)[-1],
+    !!hat_args[[2]] := si,
+    !!hat_args[[3]] := -si
+    )
+
+  if (.hyp != "none") {
+    return(mutate(df, hyp = if_else(hyp_result < 0.05, Ha, H0)))
+    } else {
+      return(df)
+      }
+  }
+
+
 
 # Hetroscadasticity test (Breusch Pagan test)(level of confidence 95%;
 # cut-off 0.05 for H0 rejection)
@@ -285,8 +255,6 @@ custom_bp <- function(df, Xt2){
 
   return(R2 * length(R2))
   }
-
-
 
 sigma_calc <- function(res) sqrt(sum((res ^ 2)) / (length(res) - 1))
 
