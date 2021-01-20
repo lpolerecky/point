96c06e2e7810d8fcbe913a7e2b46b407e07aaea6
martinschobben <m.a.n.schobben@uu.nl>
2021-01-20 19:05:53 +0100

    list collecting of quosures. introducing selecion of the statistic transformations with argument .stat.

--- R/analyse_IC.R
+++ R/analyse_IC.R
@@ -17,14 +17,23 @@
 #' @param .ion1 A character string constituting the heavy isotope ("13C").
 #' @param .ion2 A character string constituting the light isotope ("12C").
 #' @param ... Variables for grouping.
-#' @param .Xt A variable constituting the ion count rate.
-#' @param .N A variable constituting the ion counts.
-#' @param .species A variable constituting the species analysed.
-#' @param .t A variable constituting the time of the analyses.
+#' @param .Xt A variable constituting the ion count rate (defaults to
+#' variables generated with \code{read_IC()}.)
+#' @param .N A variable constituting the ion counts (defaults to variables
+#' generated with \code{read_IC()}.).
+#' @param .species A variable constituting the species analysed (defaults to
+#' variables generated with \code{read_IC()}.).
+#' @param .t A variable constituting the time of the analyses (defaults to
+#' variables generated with \code{read_IC()}.).
+#' @param .stat Select statistics (e.g. \code{c("M", "RS"}), see the tables
+#' \code{point::names_stat_Xt} and \code{point::names_stat_R} for the full
+#' selection of statistics available (default uses all statistic
+#' transformations).
 #' @param .label A character string indicating whether variable names are latex
-#' compatible. Will be extended in the future.
+#' compatible. Will be extended in the future \code{default = NULL}.
 #' @param .output A character string for output as summary statistics ("sum");
-#' statistics only ("stat"); and statistics with the original data ("complete").
+#' statistics only ("stat"); and statistics with the original data ("complete")
+#' \code{default = "sum"}.
 #' @param .zero A character string that determines whether analyses with zero
 #' count measurements will be removed from the calculations.
 #'
@@ -50,24 +59,27 @@
 #' tb_R <- stat_R(tb_pr, "13C", "12C", file.nm, .zero = TRUE)
 #'
 stat_Xt <- function(.df,..., .Xt = Xt.pr, .N = N.pr, .species = species.nm,
-                    .label = NULL, .output = "sum"){
+                    .t = t.nm, .stat = point::names_stat_Xt$name, .label = NULL,
+                    .output = "sum"){
 
   stopifnot(tibble::is_tibble(.df))
+  if (.output != "sum"  & !is.null(.label)) {
+    stop("Latex labels is not supported for complete datasets.")
+  }
 
-# Quoting the call (user-supplied expressions)
+  # Quoting the call (user-supplied expressions)
   Xt <- enquo(.Xt)
   N <- enquo(.N)
+  t <- enquo(.t)
   species <- enquo(.species)
   gr_by <- enquos(...)
 
-# New quosures
-  S_Xt <- quo_updt(my_q = Xt , pre = "S") # standard deviation ion count
-  M_Xt <- quo_updt(my_q = Xt , pre = "M") # mean ion count
-  SeM_Xt <- quo_updt(my_q = Xt , pre = "SeM") # standard error of the mean
-  hat_SeM_Xt <- quo_updt(my_q = Xt , pre = "hat_SeM") # predicted sem
+  # New quosures
+  args <- purrr::map(point::names_stat_Xt$name, ~quo_updt(Xt , pre = .x)) %>%
+    set_names(nm = paste(point::names_stat_Xt$name, "Xt", sep = "_"))
 
-# The statistics
-  args <- quos(
+  # The statistics
+  calcs <- quos(
     # number of measurements
     n(),
     # sum of ion counts (important for external reproducibility calcs)
@@ -77,7 +89,7 @@ stat_Xt <- function(.df,..., .Xt = Xt.pr, .N = N.pr, .species = species.nm,
     # standard deviation (SD) count rate
     sd(!! Xt),
     # RSD ion count rate
-    (!! S_Xt / !! M_Xt) * 1000,
+    (!! args[["S_Xt"]] / !! args[["M_Xt"]]) * 1000,
     # standard error of the mean (SE) count rate
     sd(!! Xt) / sqrt(n()),
     # predicted SD count rate
@@ -85,24 +97,29 @@ stat_Xt <- function(.df,..., .Xt = Xt.pr, .N = N.pr, .species = species.nm,
     # predicted SE count rate
     sqrt(mean(!!N) / n()),
     # reduced chi squared
-    (!! SeM_Xt  /  !! hat_SeM_Xt) ^ 2
+    (!! args[["SeM_Xt"]]  /  !! args[["hat_SeM_Xt"]]) ^ 2
     )
 
-# The statistic names (depend on user-supplied expression)
-  ls_nm <-paste(point::nm_stat_Xt$nm, as_name(Xt), sep = "_")
+  # The statistic names (depend on user-supplied expression)
+  ls_nm <- paste(point::names_stat_Xt$name, as_name(Xt), sep = "_")
 
-# To render nice latex variable names in Rmarkdown/Latex
-  ls_latex <- set_names(ls_nm, point::nm_stat_Xt$latex)
+  # Set statistic names
+  calcs <- set_names(calcs , nm = ls_nm)
 
-# Set statistic names
-  args <- set_names(args, nm = ls_nm)
+  # Stat selection
+  pos_vars <- purrr::keep(ls_nm, ~str_detect(., str_c(paste0("^", .stat), collapse = "|")))
+  neg_vars <- purrr::discard(ls_nm, ~str_detect(., str_c(paste0("^", .stat), collapse = "|")))
 
-# Evaluate expressions
+  # To render nice latex variable names in Rmarkdown/Latex
+  ls_latex <- set_names(pos_vars, nm = purrr::map_chr(.stat, ~stat_labeller(stat = .x)))
+
+  # Evaluate expressions
   df <- group_by(.df, !!! gr_by, !! species) %>%
-          eval_tidy(expr = mod_cal(.output, args = args)) %>%
-          ungroup()
+          eval_tidy(expr = mod_cal(.output, calcs = calcs)) %>%
+          ungroup() %>%
+          select(-any_of(neg_vars))
 
-# Output
+  # Output
   if (!is.null(.label)) {
     if (.label == "latex") {
     df <- mutate(
@@ -120,40 +137,48 @@ stat_Xt <- function(.df,..., .Xt = Xt.pr, .N = N.pr, .species = species.nm,
 #'
 #' @export
 stat_R <- function(.df, .ion1, .ion2, ..., .Xt = Xt.pr, .N = N.pr,
-                   .species = species.nm, .t = t.nm, .label = NULL,
+                   .species = species.nm, .t = t.nm,
+                   .stat = point::names_stat_R$name, .label = NULL,
                    .output = "sum", .zero = FALSE){
 
   stopifnot(tibble::is_tibble(.df))
+  if (.output != "sum"  & !is.null(.label)) {
+    stop("Latex labels is not supported for complete datasets.")
+  }
 
-# Quoting the call (user-supplied expressions)
+  # Quoting the call (user-supplied expressions)
   Xt <- enquo(.Xt)
   N <- enquo(.N)
   species <- enquo(.species)
   t <- enquo(.t)
   gr_by <- enquos(...)
 
-# Update quosures (heavy isotope)
+  # If t column is empty create a manual time increment
+  if (!(as_name(t) %in% colnames(.df))) {
+    .df <- group_by(.df, !!!gr_by, !!species) %>% mutate(!!t := row_number())
+  }
+
+  # Remove white space in ion names
+  .ion1 <- str_replace_all(.ion1, "\\s", "")
+  .ion2 <- str_replace_all(.ion2, "\\s", "")
+
+  # Update quosures (heavy isotope)
   Xt1 <- quo_updt(Xt, post = .ion1) # count rate
-  M_Xt1 <- quo_updt(Xt, pre = "M", post = .ion1) # mean count rate
-  S_Xt1 <- quo_updt(Xt, pre = "S", post = .ion1) # sd count rate
   N1 <- quo_updt(N, post = .ion1) # counts
 
-# Update quosures (light isotope)
+  # Update quosures (light isotope)
   Xt2 <- quo_updt(Xt, post = .ion2) # count rate
-  M_Xt2 <- quo_updt(Xt, pre = "M", post = .ion2) # mean count rate
-  S_Xt2 <- quo_updt(Xt, pre = "S", post = .ion2) # sd count rate
   N2 <- quo_updt(N, post = .ion2) # counts
 
-# Update quosures (R)
-  n_R <- quo_updt(Xt, pre = "n_R") # observations
-  M_R <- quo_updt(Xt, pre = "M_R") # mean R
-  S_R <- quo_updt(Xt, pre = "S_R") # standard deviation R
-  SeM_R <- quo_updt(Xt, pre = "SeM_R") # standard error of the mean R
-  hat_S_R <- quo_updt(Xt, pre = "hat_S_R") # predicted standard deviation R
-  hat_SeM_R <- quo_updt(Xt, pre = "hat_SeM_R") # predicted sem R
+  # Update quosures (R)
+  args <- purrr::map(
+    paste0(point::names_stat_R$name, "_R"),
+    ~quo_updt(Xt , pre = .x)
+    ) %>%
+    set_names(nm = paste(point::names_stat_R$name, "R", sep = "_"))
 
-# The statistics
-  args <- quos(
+  # The statistics
+  calcs <- quos(
     # number of measurements
     n(),
     # mean isotope ratio
@@ -161,54 +186,55 @@ stat_R <- function(.df, .ion1, .ion2, ..., .Xt = Xt.pr, .N = N.pr,
     # SD isotope ratio
     stat_SDprop(!!Xt1, !!Xt2),
     # RSD isotope ratio
-    (!!S_R / !!M_R) * 1000,
+    (!! args[["S_R"]] / !! args[["M_R"]]) * 1000,
     # SE isotope ratio
-    !!S_R / sqrt(n()),
+    !! args[["S_R"]] / sqrt(n()),
     # RSE isotope ratio
-    (!!SeM_R / !!M_R) * 1000,
+    (!! args[["SeM_R"]] / !! args[["M_R"]]) * 1000,
     # predictive SD isotope ratio
     stat_SDprop(!!N1, !!N2, predicted = TRUE),
     # predictive RSD isotope ratio
-    !!hat_S_R / !!M_R * 1000,
+    !! args[["hat_S_R"]] / !! args[["M_R"]] * 1000,
     # predictive SE isotope ratio
-    !!hat_S_R / sqrt(n()),
+    !!args[["hat_S_R"]]  / sqrt(n()),
     # predictive RSE isotope ratio
-    !!hat_SeM_R / !!M_R * 1000,
+    !! args[["hat_SeM_R"]]  / !! args[["M_R"]] * 1000,
     # reduced chi squared
-    (!!SeM_R / !!hat_SeM_R) ^ 2
+    (!! args[["SeM_R"]] / !! args[["hat_SeM_R"]]) ^ 2
     )
 
-# The statistic names (depend on user-supplied expression)
-  ls_nm <-paste(point::nm_stat_R$nm, "R", as_name(Xt), sep = "_")
+  # The statistic names (depend on user-supplied expression)
+  ls_nm <-paste(point::names_stat_R$name, "R", as_name(Xt), sep = "_")
 
-# To render nice latex variable names in Rmarkdown/Latex
-  ls_latex <- set_names(ls_nm, nm = point::nm_stat_R$latex)
+  # Set statistic names
+  calcs <- set_names(calcs, nm = ls_nm)
 
-# Set statistic names
-  args <- set_names(args, nm = ls_nm)
-
-# Extra arg in case of mutate and transmute (isotope ratio for each time step)
+  # Extra arg in case of mutate and transmute (isotope ratio for each time step)
   if (.output != "sum") {
-    args[[paste("R", as_name(Xt), sep = "_")]] <- quo(!!Xt1 / !!Xt2)
+    calcs[[paste("R", as_name(Xt), sep = "_")]] <- quo(!!Xt1 / !!Xt2)
   }
 
-# Evaluate expressions and calls
+  # Stat selection
+  pos_vars <- purrr::keep(ls_nm, ~str_detect(., str_c(paste0("^", .stat), collapse = "|")))
+  neg_vars <- purrr::discard(ls_nm, ~str_detect(., str_c(paste0("^", .stat), collapse = "|")))
+
+  # To render nice latex variable names in Rmarkdown/Latex
+  ls_latex <- set_names(pos_vars, nm = purrr::map_chr(.stat, ~stat_labeller(stat = .x)))
+
+  # Evaluate expressions and calls
   df <- .df  %>%
-# Calculate single ion count stats
     eval_tidy(expr = zeroCt_cal(.zero, .ion1, .ion2, gr_by, N, species, t)) %>%
-    stat_Xt(!!! gr_by, .Xt = !!Xt, .N = !!N, .species = !!species,
-            .output = "complete"
-            ) %>%
     cov_R(.ion1, .ion2, !!! gr_by, .species = !!species, .t = !!t) %>%
     group_by(!!! gr_by) %>%
-    eval_tidy(expr = mod_cal(.output, args = args)) %>%
-    ungroup()
+    eval_tidy(expr = mod_cal(.output, calcs = calcs)) %>%
+    ungroup() %>%
+    select(-any_of(neg_vars))
 
 # Output
   if (!is.null(.label)) {
     if (.label == "latex") {
     df <- mutate(df, R.nm = R_labeller(.ion1, .ion2, label = .label)) %>%
-      select(!!! gr_by, R = "R.nm", !!! ls_latex)
+      select(!!! gr_by, R.nm = "R.nm", !!! ls_latex)
     return(df)
     }
     }
@@ -294,12 +320,12 @@ stat_SDprop <- function(ion1, ion2, type = "sd", predicted = FALSE){
 #-------------------------------------------------------------------------------
 
 # Switch output complete dataset, stats or summary stats
-mod_cal <- function(type, args) {
+mod_cal <- function(type, calcs) {
   switch(
     type,
-    complete = call2( "mutate", expr(.), !!! args),
-    stat = call2( "transmute", expr(.), !!! args),
-    sum = call2("summarize", expr(.), !!! args),
+    complete = call2( "mutate", expr(.), !!! calcs),
+    stat = call2( "transmute", expr(.), !!! calcs),
+    sum = call2("summarize", expr(.), !!! calcs),
   )
 }
 
