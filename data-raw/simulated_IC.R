eaf1f9d633e733ce55f826618db66154c7a06bc7
martinschobben <m.a.n.schobben@uu.nl>
2021-01-21 14:26:19 +0100

    nhew simulated IC data aftr function updates

--- data-raw/simulated_IC.R
+++ data-raw/simulated_IC.R
@@ -1,4 +1,7 @@
 ## code to prepare `simulated_IC` dataset
+library(point)
+library(rlang)
+library(tidyverse)
 
 # repetition
 reps <- 15
@@ -6,56 +9,43 @@ reps <- 15
 # Types of R variation
 var_R <- c("symmetric", "asymmetric")
 
-# Varying linear trends in the ionization efficiency
+# Varying linear trends in the ionization efficiency (per mille heavy ion)
 var_T <- seq(0, 500, length.out = 6)
 
-# # varying isotope offset
+# Varying isotope offset (delta per mille)
 var_I <- seq(0, -30, length.out = 6)
 
 # Seeds for number generation
-tot_length <- length(var_T) * length(var_R) * length(var_I)
-var_seed <- 1:tot_length
+n_tot <- prod(sapply(list(var_T, var_R, var_I), length))
+var_seed <- 1:n_tot
 
 # Cross all possible parameter combinations
-sim_R.ext <- tidyr::crossing(sys = var_T, type = var_R, offsetR = var_I) %>%
-  mutate(seed = var_seed) %>%
-  transmute(params = purrr::pmap(lst(sys, type, seed, offsetR), lst))
+simu_R <- tidyr::crossing(.sys = var_T, .type = var_R, .devR = var_I) %>%
+  mutate(.seed = var_seed) %>%
+  transmute(params = purrr::pmap(lst(.sys, .type, .seed, .devR), lst))
 
+# Function to map over params
+map_R <- function(fn, args) {
+  exec(fn, !!! args, .reps = reps, .ion1 = "13C", .ion2 = "12C")
+  }
 
-sim_IC <- purrr::map2_dfr(
-  rep("sim_R", tot_length),
-  sim_R.ext$params,
-  function(fn, args) {
-    exec(fn, !!! args, reps = reps, ion1 = "13C", ion2 = "12C", baseR = 0)
-    }
+# Execute
+simu_IC <- purrr::map2_dfr(
+  rep("simu_R", n_tot),
+  simu_R$params,
+  map_R
   )
 
+# Save data for application paper
+saveRDS(simu_IC, "/home/amandus/Documents/work/manuscripts/Schobbenetal_SIMS_carb_stds/data/sim_IC.rds")
 
-usethis::use_data(sim_IC, overwrite = TRUE, compress = "xz")
-
-# limited dataset only to show range in systematic of an isotopically
-# ideal substance but different systematic trends
-sim_IC_systematic <- sim_IC %>%
-  filter(
-    trend == "linear trend (var: 0)" |
-      trend == "linear trend (var: 500)"
-    )
-
-usethis::use_data(sim_IC_systematic, overwrite = TRUE, compress = "xz")
-
-# limited dataset only to show range in systematic and random R trends for
-sim_IC_extremes <- sim_IC  %>%
-  filter(trend == "linear trend (var: 0)" |
-         trend == "linear trend (var: 500)",
-         repetition == 1
-         )
-
-usethis::use_data(sim_IC_extremes, overwrite = TRUE)
-
-# limited dataset only to show range in systematic of an isotopically
-# ideal substance
-sim_IC_ideal <- sim_IC_extremes %>%
-  filter(simulation == "ideal")
-
-usethis::use_data(sim_IC_ideal, overwrite = TRUE)
+# Limited data internally only to show functionality of eval_diag()
+simu_IC <- filter(
+  simu_IC,
+  trend.nm == 500,
+  type.nm == "asymmetric",
+  force.nm %in% c(-30, -12, 0),
+  spot.nm %in% 1:5
+  )
+usethis::use_data(simu_IC, overwrite = TRUE, compress = "xz")
 
