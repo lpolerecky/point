9ca5645d47e4508679827a2320f1db138fa4b601
martinschobben <m.a.n.schobben@uu.nl>
2021-03-24 11:01:40 +0100

    updates

--- tests/testthat/test_read_raw.R
+++ tests/testthat/test_read_raw.R
@@ -5,27 +5,25 @@ library(point)
 sv1 <- point_example("2018-01-19-GLENDON")
 
 # Processing raw ion count data
-tb.pr <- suppressWarnings(cor_IC(read_IC(sv1), N.rw, t.rw, det_type.mt,
-                                 deadtime = 44, thr_PHD = 50))
+tb_pr <- suppressWarnings(cor_IC(read_IC(sv1)))
 
 #-------------------------------------------------------------------------------
 # Variable class checks and compatibility of txt and stat files
 #-------------------------------------------------------------------------------
 # Error when directory argument class is wrong
-test_that("read_IC input error", {
+testthat::test_that("read_IC input error", {
   expect_error(read_IC(1))
   expect_error(read_IC(1L))
   })
 
 
-# Warning when zero count exist
-test_that("zero counts", {
-  expect_warning(
-    stat_R(tb.pr, Xt.pr, N.pr, species.nm, ion1 = "13C",
-           ion2 = "12C", file.nm, zero = TRUE))
-  })
+# # Warning when zero count exist
+# testthat::test_that("zero counts", {
+#   expect_warning(
+#     stat_R(tb_pr, "13C", "13C 14N", file.nm, .zero = TRUE))
+#   })
 
 # Testing the class of the retrieved data
-test_that("read_IC creates a tibble", {
-  expect_is(read_IC(sv1), "tbl_df")
+testthat::test_that("read_IC creates a tibble", {
+  expect_is(suppressWarnings(read_IC(sv1)), "tbl_df")
   })
